7a21f6e9564e6d2bcedd9e27e8031838d73c8604
fabriceadou5@gmail.com <fabriceadou5@gmail.com>
2025-03-06 08:19:24 +0000

    Ajout de la section Home et de tailwind CSS

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import bgimage from '../../assets/bgimage.png'
+
+const Home = () => {
+  return (
+    // Image arriere plan
+    <div id='home' className='bg-[#090674] lg:h-[600px] h-fit m-auto pt-[60px] lg:pt-[0px] lg:px-[150px]
+    px-[20px] flex justify-center items-center bg-cover bg-center'
+    style={{ backgroundImage: `url(${bgimage})` }}>
+
+      {/* Texte au centre */}
+      <div className='flex flex-col justify-center items-center text-white'>
+        <div data-aos="zoom-up" data-aos-delay="300" className='flex flex-col justify-center items-center'>
+          <h1 className='text-[50px]'>Je suis</h1>
+          <h1 className='text-[50px] font-extrabold'>ADOU NIANGORAN FABRICE</h1>
+        </div>
+        <h1 className='text-[30px]' data-aos="fade-up" data-aos-delay="700">Développeur Full-stack et Mobile</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
