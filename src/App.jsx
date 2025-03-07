d118e60b64b49870b8d3bc356a92052c5c83efba
fabriceadou5@gmail.com <fabriceadou5@gmail.com>
2025-03-07 02:57:35 +0000

    modification du fichier App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
-import Project from './components/Projects/Project';
 import Exprience from './components/Experience/Exprience';
 
 const App = () => {
@@ -29,7 +28,7 @@ const App = () => {
       <About />
       <Skills />
       <Exprience />
-      <Project />
+      
     </>
   )
 }
